62f16e91552dafe99edc528dedf63020c353520c
Jiyeon Seo <jiyeonseo@fb.com>
2018-12-13 17:52:06 +0900

    add breathFirstSearch

--- README.md
+++ README.md
@@ -1,33 +1,45 @@
-# Datastructures and Algorithms in Javascript practice 
+# Datastructures and Algorithms in Javascript practice
 
-## Queue 
-FIFO 
-- enqueue 
+## Queue
+
+FIFO
+
+- enqueue
 - dequeue
-- peek 
-- length 
+- peek
+- length
 - isEmpty
 
 ## Stack
-FILO 
+
+FILO
+
 - push
 - pop
 - peek
-- length 
+- length
 - isEmpty
 
-## Linked List 
-Node 
-- head
-- tail 
-- length 
-- push 
-- pop
-- get 
-- delete 
-- isEmpty 
+## Linked List
 
-## Graph Data Structure 
+Node
+
+- head
+- tail
+- length
+- push
+- pop
+- get
+- delete
+- isEmpty
+
+## Graph Data Structure
+
 a collection of nodes (a.k.a vertices)
 Nodes may point to other nodes, known as edges
+
 - Node / Edge
+
+### Breadth first search
+
+a graph search algorithm that starts at one node and visits neighboring nodes as widely as possible before going further down any other path.
--- graph_data_structure/index.js
+++ graph_data_structure/index.js
@@ -1,63 +1,117 @@
+const { createQueue } = require("../queue/index.js");
+
 function createNode(key) {
-    const neighbors = [];
-    return {
-        key,
-        neighbors,
-        addNeighbors(node) {
-            neighbors.push(node);
-        }
+  const neighbors = [];
+  return {
+    key,
+    neighbors,
+    addNeighbors(node) {
+      neighbors.push(node);
     }
+  };
 }
 
 function createGraph(directed = false) {
-    const nodes = [];
-    const edges = [];
-    return {
-        directed,
-        nodes,
-        edges,
-        addNode(key) {
-            nodes.push(createNode(key));
-        },
-        getNode(key) {
-            return nodes.find(node => node.key === key);
-        },
+  const nodes = [];
+  const edges = [];
+  return {
+    directed,
+    nodes,
+    edges,
+    addNode(key) {
+      nodes.push(createNode(key));
+    },
+    getNode(key) {
+      return nodes.find(node => node.key === key);
+    },
 
-        addEdge(node1key, node2key) {
-            const node1 = this.getNode(node1key);
-            const node2 = this.getNode(node2key);
+    addEdge(node1key, node2key) {
+      const node1 = this.getNode(node1key);
+      const node2 = this.getNode(node2key);
 
-            node1.addNeighbors(node2);
-            edges.push(`${node1key}-${node2key}`)
+      node1.addNeighbors(node2);
+      edges.push(`${node1key}-${node2key}`);
 
-            if (!directed) {
-                node2.addNeighbors(node1);
-            }
-        },
+      if (!directed) {
+        node2.addNeighbors(node1);
+      }
+    },
 
-        print() {
-            return nodes.map(({ key, neighbors }) => {
-                let result = key;
-                if (neighbors.length) {
-                    result += ` => ${neighbors.map(neighbor => neighbor.key).join(' ')}`
-                }
-
-                return result;
-            })
+    print() {
+      return nodes.map(({ key, neighbors }) => {
+        let result = key;
+        if (neighbors.length) {
+          result += ` => ${neighbors.map(neighbor => neighbor.key).join(" ")}`;
         }
+
+        return result;
+      });
+    },
+
+    // search algorithm
+    breathFirstSearch(startingNodeKey, visitFn) {
+      const startingNode = this.getNode(startingNodeKey);
+
+      const visited = nodes.reduce((acc, node) => {
+        acc[node.key] = false;
+        return acc;
+      }, {}); // init visited as all false; later visited item would be changed as true
+
+      const queue = createQueue();
+      queue.enqueue(startingNode);
+
+      while (!queue.isEmpty()) {
+        const currentNode = queue.dequeue();
+        if (!visited[currentNode.key]) {
+          visitFn(currentNode);
+          visited[currentNode.key] = true;
+        }
+
+        currentNode.neighbors.forEach(node => {
+          if (!visited[node.key]) {
+            queue.enqueue(node);
+          }
+        });
+      }
     }
+  };
 }
 
 const graph = createGraph(true);
-graph.addNode('A');
-graph.addNode('B');
-graph.addNode('C');
-graph.addNode('D');
+// graph.addNode("A");
+// graph.addNode("B");
+// graph.addNode("C");
+// graph.addNode("D");
 
-graph.addEdge('A', 'B');
-graph.addEdge('B', 'A');
+// graph.addEdge("A", "B");
+// graph.addEdge("B", "A");
 
-graph.addEdge('A', 'C');
-graph.addEdge('A', 'D');
+// graph.addEdge("A", "C");
+// graph.addEdge("A", "D");
 
-console.log(graph.print()); // [ 'A => B C D', 'B => A', 'C', 'D' ]
+// console.log(graph.print()); // [ 'A => B C D', 'B => A', 'C', 'D' ]
+
+const nodes = ["a", "b", "c", "d", "e", "f"];
+const edges = [
+  ["a", "b"],
+  ["a", "e"],
+  ["a", "f"],
+  ["b", "d"],
+  ["b", "e"],
+  ["c", "b"],
+  ["d", "c"],
+  ["d", "e"]
+];
+
+nodes.forEach(node => {
+  graph.addNode(node);
+});
+
+edges.forEach(nodes => {
+  graph.addEdge(...nodes);
+});
+
+console.log(graph.print());
+graph.breathFirstSearch("a", node => {
+  console.log(node.key);
+});
